999a46ce6b5886a4a7598603da64cd4705266d60
Anthony Rojas <anthony.rojas095@gmail.com>
2023-02-12 20:42:17 -0800

    Initial commit

---
+++ test/serverless_price_scraper.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessPriceScraper from '../lib/serverless_price_scraper-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless_price_scraper-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessPriceScraper.ServerlessPriceScraperStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
